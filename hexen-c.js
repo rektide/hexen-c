6f9dedd3264e41aa4defb8a7e411f52547ba0e41
rektide <rektide@voodoowarez.com>
2013-10-04 23:25:16 -0400

    Move from zero base to one base counting. Accept lower case. Add a dummy test.

--- hexen-c.js
+++ hexen-c.js
@@ -2,14 +2,19 @@ var data= []
 function inz(datum){data.push(new Buffer(datum).toString())}
 process.stdin.on("data",inz)
 process.stdin.on("end",function(){
-	var hexes= /[0-9A-F]/,
+	var hexes= /[0-9A-Fa-f]/,
 	  agg= {}
 	function eventTrigger(n){
-		agg[n]= (agg[n]||-1)+1
+		if(!isNaN(n))
+			n= parseInt(n)
+		else
+			n= n.toLowerCase()
+		agg[n]= (agg[n]||0)+1
 	}
 	for(var i in data)
-		for(var j in data[i])
+		for(var j in data[i]){
 			if(hexes.test(data[i][j]))
 				eventTrigger(data[i][j])
+		}
 	console.log(agg)
 })
